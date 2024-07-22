9e41bc24a628718a0b392a2656855bcf4d23b916
Joselow <Joselow>
2024-07-22 15:21:35 -0500

    [docs]

---
+++ docs/assets/NotFoundView-5f3f21d7.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o,a as s}from"./index-471bf63f.js";const c={},n={class:"flex items-center justify-center h-screen"},a=s("h1",{class:"text-4xl font-bold"},"404 - Página no encontrada",-1),r=[a];function _(d,i){return t(),o("div",n,r)}const l=e(c,[["render",_]]);export{l as default};
